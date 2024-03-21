d23021140d8fd9077447f6a0740c891367af5ce0
prasanthGitpractice <galiprasanth6@gmail.com>
2024-03-21 21:16:07 +0530

    master2

--- src/main/java/TC00_PreReq/Script.java
+++ src/main/java/TC00_PreReq/Script.java
@@ -1,10 +1,7 @@
-package stashAndPop;
+package TC00_PreReq;
 
 public class Script {
-
 	public static void main(String[] args) {
-
-		System.out.println("Master1");
+		System.out.println("Tc00");
 	}
-
 }
